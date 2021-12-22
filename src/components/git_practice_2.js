ad2a98cb539f2e64a5c4780383cf2ab75abf299a
klimenko3007 <ek1830@tobii.com>
2021-12-22 11:58:57 +0100

    Commit 8

--- src/components/git_practice_2.js
+++ src/components/git_practice_2.js
@@ -1,3 +1,4 @@
 const newCommit = "And here is to make another commit"
 const commirFive = "Another commit"
 const commitSeven = "An another commit"
+const commitEight = "An another commit"
---
+++ src/components/newfile.js
@@ -0,0 +1,1 @@
+const Commit = "new file"
